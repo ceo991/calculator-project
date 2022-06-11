97dc0cdedef8a556d81ed8d03ad50014be24db07
ceo991 <ceo991@hotmail.com>
2022-06-11 22:26:41 +0300

    code cleaned up

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ class App extends Component{
     super(props)
     this.state={
       calculation: "",
-      prevCalc: "",
-      evaluated: false,
-      operands: [],
-      operations: []
+      prevCalc: ""
     }
     this.entryHandler=this.entryHandler.bind(this)
     this.initiliaize=this.initiliaize.bind(this)
@@ -25,7 +22,6 @@ class App extends Component{
     let element = e.target.value
     let tempCalc = this.state.calculation.toString()
     let tempPrev = this.state.prevCalc.toString()
-    let tempOperations = this.state.operations
     
     if(element === "/" || element === "*" || element === "+" || element === "-" ){
 
@@ -78,7 +74,6 @@ class App extends Component{
       if((element==="/" || element==="*" || element==="+")&&(tempCalc.slice(-1)==="/" || tempCalc.slice(-1)==="*" || tempCalc.slice(-1)==="-" || tempCalc.slice(-1)==="+")){
 
         let tempArr = [...tempCalc];
-        tempArr = tempArr.filter((el)=>el !== ",");
         let checkArr = tempArr.slice(-2)
         if(checkArr.length>1&&checkArr.includes("-")){
           tempArr.splice(tempArr.indexOf(checkArr[0]),2,element)
@@ -89,30 +84,15 @@ class App extends Component{
         tempArr.toString().split("")
         tempArr.pop()
         tempArr.push(element)
-        tempOperations.splice(tempOperations.indexOf(tempPrev),1,element)   
-        tempArr = tempArr.filter((el)=>el !== ",");
         tempArr.join("")
         tempCalc=tempArr
-        this.setState({calculation: tempCalc,prevCalc: tempPrev,operations:tempOperations})
+        this.setState({calculation: tempCalc,prevCalc: tempPrev})
         return
       }
       if(element==="-" && tempCalc.slice(-1)==="-")return
       tempCalc += element
-    }
-    
-    if(element==="/"||element==="*" ||element==="-" ||element==="+" ){
+    }   
 
-     tempOperations = [...this.state.operations, element]
-
-      let tempOperands =[...this.state.operands,this.state.prevCalc]
-      tempOperands=tempOperands.filter(num=>num !== 0)
-      tempOperands=tempOperands.filter((el)=>el!=="/")
-      tempOperands=tempOperands.filter((el)=>el!=="*")
-      tempOperands=tempOperands.filter((el)=>el!=="-")
-      tempOperands=tempOperands.filter((el)=>el!=="+")
-      this.setState({operations:tempOperations})
-      this.setState({operands:tempOperands})
-    }
     if(tempCalc[0] ==="/"||tempCalc[0] ==="*"||tempCalc[0] ==="+"){
       let arr = [...tempCalc]
       arr.toString().split("")
@@ -121,29 +101,20 @@ class App extends Component{
       tempCalc=arr.toString()
     }
     
-    this.setState({calculation: tempCalc.split("").filter((el)=>el !== ",").join(""),prevCalc: tempPrev})
+    this.setState({calculation: tempCalc,prevCalc: tempPrev})
   }
   
   initiliaize(){
     this.setState({calculation: "",prevCalc: "",operands:[] ,operations:[], evaluated:false})
   }
   
-   evaluateExpression(fn) {
+  evaluateExpression(fn) {
     return new Function('return ' + fn)();
   }
   
   evaluate(){
-    let tempArr = [...this.state.operands]
-    let tempOperations =[...this.state.operations]
-    tempArr.push(this.state.prevCalc)
-    tempArr=tempArr.filter((el)=>el!=="/")
-    tempArr=tempArr.filter((el)=>el!=="*")
-    tempArr=tempArr.filter((el)=>el!=="-")
-    tempArr=tempArr.filter((el)=>el!=="+")
-    tempArr = tempArr.filter(el=>el!=="")
-    tempArr = tempArr.map((num)=>parseFloat(num))
     let result = this.evaluateExpression(this.state.calculation)
-    this.setState({evaluated:true,operands:tempArr,calculation:result.toString(),prevCalc: result})
+    this.setState({calculation:result.toString(),prevCalc: result})
   }
   
   render(){
